6a83e4c8c5c5d6f006b3eea871d3653151384b58
irangelova <ivetarangelova@gmail.com>
2025-01-11 23:23:11 +0100

    Solutions to tasks from stacks and queues lecture

---
+++ lists_as_stacks_and_queues/E_09_key_revolver_v2.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+price_of_bullet = int(input())
+size_gun_barrel = int(input())
+bullets = [int(bullet) for bullet in input().split()]
+locks = deque(int(lock) for lock in input().split())
+value_of_intelligence = int(input())
+bullets_shot = 0
+
+while bullets and locks:
+    remaining_bullets = size_gun_barrel
+
+    while remaining_bullets > 0 and bullets and locks:  # Added bullets and locks checks
+        current_lock = locks[0]
+        current_bullet = bullets.pop()
+        bullets_shot += 1
+
+        if current_bullet >= current_lock:  # Fixed comparison (bullet should be >= lock)
+            locks.popleft()
+            print("Bang!")
+        else:
+            print("Ping!")
+
+        remaining_bullets -= 1
+
+        if remaining_bullets == 0 and bullets:  # Moved reload check here
+            print("Reloading!")
+
+if locks:  # Simplified the end condition checks
+    print(f"Couldn't get through. Locks left: {len(locks)}")
+else:
+    money_earned = value_of_intelligence - (bullets_shot * price_of_bullet)
+    print(f"{len(bullets)} bullets left. Earned ${money_earned}")
