1220cb49d174d9ad5d51092ceecd6ba96eb671fa
irangelova <ivetarangelova@gmail.com>
2025-01-18 17:34:50 +0100

    Solutions to tasks from multidimensional lists lecture

---
+++ 03_multidimensional_lists/E_03_2x2_squares_in_matrix.py
@@ -0,0 +1,20 @@
+rows, columns = [int(el) for el in input().split()]
+matrix = []
+
+matrices_found = 0
+
+for _ in range(rows):
+    row_data = list(input().split())
+    matrix.append(row_data)
+
+for row_index in range(len(matrix) - 1):
+    for col_index in range(len(matrix[row_index]) - 1):
+        set_el = set()
+        set_el.add(matrix[row_index][col_index])
+        set_el.add(matrix[row_index][col_index + 1])
+        set_el.add(matrix[row_index + 1][col_index])
+        set_el.add(matrix[row_index + 1][col_index + 1])
+        if len(set_el) == 1:
+            matrices_found += 1
+
+print(matrices_found)
