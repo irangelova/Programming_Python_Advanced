32e24bacb04aff633c890c84479dcbd63217627e
irangelova <ivetarangelova@gmail.com>
2025-01-18 17:28:18 +0100

    Solutions to tasks from multidimensional lists lecture

---
+++ 03_multidimensional_lists/E_02_diagonal_difference.py
@@ -0,0 +1,15 @@
+rows = int(input())
+matrix = []
+
+for _ in range(rows):
+    row_data = list(input().split())
+    matrix.append(row_data)
+
+sum_primary_diagonal = 0
+sum_secondary_diagonal = 0
+
+for row_index in range(len(matrix)):
+    sum_primary_diagonal += int(matrix[row_index][row_index])
+    sum_secondary_diagonal += int(matrix[row_index][rows - row_index - 1])
+
+print(abs(sum_primary_diagonal - sum_secondary_diagonal))
