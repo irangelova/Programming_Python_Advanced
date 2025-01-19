35d225a42840eefa82109a05671d7311a92b0437
irangelova <ivetarangelova@gmail.com>
2025-01-19 17:03:18 +0100

    Solutions to tasks from multidimensional lists lecture

--- 03_multidimensional_lists/E_02_diagonal_difference.py
+++ 03_multidimensional_lists/E_02_diagonal_difference.py
@@ -1,9 +1,11 @@
 rows = int(input())
-matrix = []
+#matrix = []
 
-for _ in range(rows):
-    row_data = list(input().split())
-    matrix.append(row_data)
+# for _ in range(rows):
+#     row_data = list(input().split())
+#     matrix.append(row_data)
+
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
 sum_primary_diagonal = 0
 sum_secondary_diagonal = 0
---
+++ 03_multidimensional_lists/E_05_matrix_of_palindromes.py
@@ -0,0 +1,4 @@
+rows, columns = [int(x) for x in input().split()]
+
+for _ in range(rows):
+    row_data = []
