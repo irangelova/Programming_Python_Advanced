7971f8c608836c48dca3826eeee73a2cd87256ac
irangelova <ivetarangelova@gmail.com>
2025-01-19 17:00:06 +0100

    Solutions to tasks from multidimensional lists lecture

--- 03_multidimensional_lists/E_01_diagonals.py
+++ 03_multidimensional_lists/E_01_diagonals.py
@@ -14,9 +14,13 @@ for row_index in range(len(matrix)):
     sum_primary_diagonal += matrix[row_index][row_index]
     primary_diagonal.append((matrix[row_index][row_index]))
 
+#primary_diagonal = [matrix[row_index][row_index] for row_index in range(len(matrix))]
+
 for row_index in range(len(matrix)):
     sum_secondary_diagonal += matrix[row_index][rows - row_index - 1]
     secondary_diagonal.append(matrix[row_index][rows - row_index - 1])
+
+#primary_diagonal = [matrix[row_index][rows - row_index - 1] for row_index in range(len(matrix))]
 
 print(f"Primary diagonal: {', '.join(str(el) for el in primary_diagonal)}. Sum: {sum_primary_diagonal}")
 print(f"Secondary diagonal: {', '.join(str(el) for el in secondary_diagonal)}. Sum: {sum_secondary_diagonal}")
---
+++ 03_multidimensional_lists/E_04_maximal_sum.py
@@ -0,0 +1,34 @@
+import sys
+
+rows, columns = [int(el) for el in input().split()]
+matrix = []
+
+max_sum = -sys.maxsize
+max_square = []
+
+for _ in range(rows):
+    row_data = [int(el) for el in input().split()]
+    matrix.append(row_data)
+
+for row_index in range(len(matrix) - 2):
+    for col_index in range(columns - 2):
+        current_el = matrix[row_index][col_index]
+        el_to_right = matrix[row_index][col_index + 1]
+        el2_to_right = matrix[row_index][col_index + 2]
+        el_below = matrix[row_index + 1][col_index]
+        el_diagonally = matrix[row_index + 1][col_index + 1]
+        el_2right_bellow = matrix[row_index + 1][col_index + 2]
+        el_2below = matrix[row_index + 2][col_index]
+        el_2below_right = matrix[row_index + 2][col_index + 1]
+        el2_diagonally = matrix[row_index + 2][col_index + 2]
+        current_sum = current_el + el_to_right + el2_to_right + el_below + el_diagonally + el_2right_bellow + el_2below + el_2below_right + el2_diagonally
+        if current_sum > max_sum:
+            max_sum = current_sum
+            max_square = [[current_el, el_to_right, el2_to_right],
+                          [el_below, el_diagonally, el_2right_bellow],
+                          [el_2below, el_2below_right, el2_diagonally]]
+
+print(f"Sum = {max_sum}")
+print(*max_square[0])
+print(*max_square[1])
+print(*max_square[2])
